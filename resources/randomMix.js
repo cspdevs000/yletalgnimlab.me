efe43a446e28787a793e77b4f5dfb9f592d6e8a8
cspdevs000 <cspdevs000@gmail.com>
2022-01-15 22:49:43 -0500

    1-15 changes

--- resources/randomMix.js
+++ resources/randomMix.js
@@ -38,32 +38,32 @@ everyMix.push(deflector1Mix);
 const treasureTeeth1Mix = new Mix('treasure teeth', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/791158561&color=%23ff5500&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=true', 'https://treasureteeth.bandcamp.com/');
 everyMix.push(treasureTeeth1Mix);
 
-const deflector2Mix = new Mix('deflector', 'https://www.youtube.com/embed/ivuGkJ8_ocA', 'https://deflector.bandcamp.com/');
-everyMix.push(deflector2Mix);
-
 const pahsMix = new Mix('pamela and her sons', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/280920777&color=%236d574c&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=true', 'https://pamelaandhersons.bandcamp.com/');
 everyMix.push(pahsMix);
 
 const isabellaMix = new Mix('isabella', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/968495794&color=%236d574c&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=true', 'https://networkerror.bandcamp.com/');
 everyMix.push(isabellaMix);
 
-const syanideMix = new Mix('syanide', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/956067706&color=%236c6864&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=true', 'https://syanide.bandcamp.com/');
-everyMix.push(syanideMix);
-
 // add new mixes here::
 
-
-
-// select a mix randomly 
 let randomIndex = Math.floor(Math.random() * everyMix.length);
-
-
-// change the source of the iframe, name display, and link, and toggle is Slected
 document.getElementById("sc").setAttribute("src", everyMix[randomIndex].source);
-
 document.getElementById("artistLink").innerHTML = everyMix[randomIndex].artist;
-
 document.getElementById("artistLink").setAttribute("href", everyMix[randomIndex].link);
+// select a mix randomly 
+
+function randomizeMix() { 
+    let newRandomIndex = Math.floor(Math.random() * everyMix.length);
+    console.log(newRandomIndex)
+    if (newRandomIndex == randomIndex) {
+        newRandomIndex = randomIndex + 1;
+    } 
+    document.getElementById("sc").setAttribute("src", everyMix[newRandomIndex].source);
+    document.getElementById("artistLink").innerHTML = everyMix[newRandomIndex].artist;
+    document.getElementById("artistLink").setAttribute("href", everyMix[newRandomIndex].link);
+    // change the source of the iframe, name display, and link, and toggle is Slected
+    randomIndex = newRandomIndex;
+}
 
 
 // change attributes if link is a youtube or soundcloud ++++ mix, add a tag to the class
